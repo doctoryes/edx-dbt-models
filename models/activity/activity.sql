5251e72f9fb37bd49275e88f868cfd4e478e5534
Luke Davis <mail@lukedav.is>
2019-05-24 14:14:31 -0400

    Adding illustrative enterprise example for data cleanup day

---
+++ models/activity/activity.sql
@@ -0,0 +1,15 @@
+--User activities within a course.
+with enterprise_users as
+(
+  select a.user_id, a.enterprise_customer_id as enterprise_id, b.name as enterprise_name
+  from app_data.wwc.enterprise_enterprisecustomeruser a
+  left join app_data.wwc.enterprise_enterprisecustomer b on a.enterprise_customer_id = b.uuid
+)
+select
+  f.user_id, e.enrollment_id, e.enroll_timestamp, u.enterprise_id, u.enterprise_name,
+  f.course_id, f.date, f.cnt_active_activity
+from business_intelligence.production.user_activity_engagement_daily f
+--Filter to only activities from enterprise enrollments.
+inner join {{ ref('enrollment') }} e using(user_id, course_id)
+left join enterprise_users u using(user_id)
+where date between '2019-01-01' and '2019-01-07'
